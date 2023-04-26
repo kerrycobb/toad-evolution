1339adc809e13b130e44a1f08e9f2ec18b976312
kerrycobb <cobbkerry@gmail.com>
2023-04-25 23:07:08 -0500

    Fix merge.fish

--- stacks/merge.fish
+++ stacks/merge.fish
@@ -9,13 +9,13 @@ cp $inp/plate1/*.fq.gz $out
 cp $inp/plate2/*.fq.gz $out 
 cp $inp/plate3/*.fq.gz $out 
 
-for i in plate2/kac232 plate2/kac161 plate2/kac187 plate2/kac211 plate2/kac221
+set duplicates plate2/kac232 plate2/kac161 plate2/kac187 plate2/kac211 plate2/kac221
 
-set id (string split -m 1 $i /)[1]
-rm $out/$id.*
-cat $inp/$i_1.1.fq.gz     $inp/$i_2.1.fq.gz     > $out/$id.1.fq.gz
-cat $inp/$i_1.2.fq.gz     $inp/$i_2.2.fq.gz     > $out/$id.2.fq.gz
-cat $inp/$i_1.rem.1.fq.gz $inp/$i_2.rem.1.fq.gz > $out/$id.rem.1.fq.gz
-cat $inp/$i_1.rem.2.fq.gz $inp/$i_2.rem.2.fq.gz > $out/$id.rem.2.fq.gz
-
+for i in $duplicates 
+  set sample (string split / $i )[2]
+  rm $out/$sample*
+  cat $inp/{$i}_1.1.fq.gz     $inp/{$i}_2.1.fq.gz     > $out/$sample.1.fq.gz
+  cat $inp/{$i}_1.2.fq.gz     $inp/{$i}_2.2.fq.gz     > $out/$sample.2.fq.gz
+  cat $inp/{$i}_1.rem.1.fq.gz $inp/{$i}_2.rem.1.fq.gz > $out/$sample.rem.1.fq.gz
+  cat $inp/{$i}_1.rem.2.fq.gz $inp/{$i}_2.rem.2.fq.gz > $out/$sample.rem.2.fq.gz
 end
