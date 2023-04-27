021da7045912718956d8f39d28f768ee27ad30e4
kerrycobb <cobbkerry@gmail.com>
2023-04-27 10:33:06 -0500

    Get rid of old unnedded bgc and structure files

--- bgc/clust-90-missing-0.9-exclude-1/bgc.R
+++
@@ -1,7 +0,0 @@
-library("ClineHelpR")
-
-setwd("~/Desktop/toad-project/bgc/")
-
-bgcData <- combine_bgc_output(
-  results.dir="clust-90-missing-0.9-exclude-1",
-  prefix="clust-90-missing-0.9-exclude-1")
