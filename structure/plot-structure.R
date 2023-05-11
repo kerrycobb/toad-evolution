ae30b06e650544cab75855465375c91aed7db1e4
Kerry Cobb <cobbkerry@gmail.com>
2023-05-11 17:06:57 -0500

    edits to structure plotting

--- structure/plot-structure.R
+++ structure/plot-structure.R
@@ -5,22 +5,22 @@ library(pophelper)
 library(gridExtra)
 library(ggplot2)
 
+colors <- readLines("../colors.txt")
+dataPath <- "../sample-data.csv"
 width <- 12 
 height <- 3 
 
 parser <- ArgumentParser()
 parser$add_argument("-k", "--selectK", default=NULL)
-# parser$add_argument("-l", "--labels", default=NULL)
-parser$add_argument("-c", "--colors")
+parser$add_argument("-l", "--labels", default=NULL)
+parser$add_argument("--showlegend", action='store_true', default=FALSE)
 parser$add_argument("-p", "--popmap", default=NULL)
 parser$add_argument("--showindlabel", action='store_true', default=FALSE)
 parser$add_argument("directory", nargs=1)
-parser$add_argument("colors", nargs=1)
 args <- parser$parse_args()
 
 dir <- gsub("/$", "", args$directory)
 name <- gsub("^out-", "", dir) 
-colors <- readLines(args$colors)
 
 # Read in data files
 files <- list.files(dir, full.names=TRUE)
@@ -39,14 +39,25 @@ sub_t <- t[(nrows_k1 + 1): nrow(t), ]
 # Align and merge subset of files
 aligned <- alignK(qlist[(nrows_k1+1):length(qlist)])
 merged <- mergeQ(aligned)
+mergedCopy <- merged
+
+# Replace sample ids with a project id
+mergedIds <- names(merged)
+for (i in mergedIds){
+  sampleData <- read.csv(dataPath)
+  sampleData <- sampleData[!duplicated(sampleData$sample_id),] 
+  sampleDataMerge <- merge(merged[[i]], sampleData, by.x=0, by.y="sample_id", all.x=TRUE) 
+  sampleDataMerge$proj_id <- gsub("^p", "", sampleDataMerge$proj_id)
+  rownames(merged[[i]]) <- sampleDataMerge$proj_id 
+}
 
 ################################################################################
 # If not K given, plot all runs, and output merged plot for each K
 if (is.null(args$selectK)) {
 
   # Write merged qmatrix to csv file
-  for (n in names(merged)) {
-    write.csv(merged[[n]], paste0("qmats/", name, "-K-", n, ".csv"))
+  for (n in names(mergedCopy)) {
+    write.csv(mergedCopy[[n]], paste0("qmats/", name, "-K-", n, ".csv"))
   }
 
   # Evanno
@@ -82,11 +93,12 @@ if (is.null(args$selectK)) {
       returnplot=F,
       clustercol=colors,
       splab=paste0("K=", names(merged)),
-      sortind="Cluster1",
+      # sortind="Cluster1",
       showindlab=args$showindlab,
       showticks=args$showindlab,
       useindlab=TRUE,
       sharedindlab=TRUE,
+      indlabsize=1,
     #   exportplot=F, 
     #   basesize=11,
   )
@@ -96,11 +108,19 @@ if (is.null(args$selectK)) {
 } else {
   k <- args$selectK
 
-  sampleData <- read.csv("../sample-data.csv")
-  sampleData <- sampleData[!duplicated(sampleData$sample_id),] 
-  sampleDataMerge <- merge(merged[[k]], sampleData, by.x=0, by.y="sample_id", all.x=TRUE) 
-  sampleDataMerge$proj_id <- gsub("^_", "", sampleDataMerge$proj_id)
- 
+
+
+  # Determine if legend should be shown
+  if (!is.null(args$labels) || args$showlegend ) {
+    showlegend <- TRUE
+  } else {
+    showlegend <- FALSE }
+  # Change legend labels if given
+  if (!is.null(args$labels)) {
+    labels <- strsplit(args$labels, ",")[[1]]
+  } else {
+    labels <- NA }
+    
   path <- paste0("plots/", name, "-K-", k, ".pdf")
 
   ##############################################################################
@@ -118,19 +138,22 @@ if (is.null(args$selectK)) {
       useindlab=TRUE,
       indlabsize=10,
       splabsize=0,
+      showlegend=showlegend,
+      legendlab=labels,
+      legendtextsize=12,
+      legendkeysize=10,
       # ticksize=0.1,
       # indlabspacer=0.25,
       # barbordercolour="black",
     )
 
   ##############################################################################
-  # Make plot, grouping by label
+  # If popmap given, make plot, grouping by label
   } else {
 
     popmap <- read.table(args$popmap, header=FALSE, sep = "\t")
     pops <- merge(merged[k], popmap, by.x=0, by.y="V1", all.x=TRUE, all.y=FALSE)
     pops <- pops["V2"]  
-    rownames(merged[[k]]) <- sampleDataMerge$proj_id 
 
     p <- plotQ(
       merged[k], 
@@ -147,6 +170,10 @@ if (is.null(args$selectK)) {
       indlabsize=9,
       indlabvjust=1,
       splabsize=0,
+      showlegend=showlegend,
+      legendlab=labels,
+      legendtextsize=12,
+      legendkeysize=10,
       # splab="",
       # pointsize=6,
       # linesize=7,
